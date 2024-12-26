3105f723a840f8db74772b3b640623facaa1363b
Leon-kings <leonakingeneye2002@gmail.com>
2024-12-26 11:00:33 -0800

    changes on branding

--- src/universal/packages/Package.jsx
+++ src/universal/packages/Package.jsx
@@ -9,40 +9,41 @@ export default function Package() {
         </h2>
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           <img
-            alt="Transistor"
-            src="https://tailwindui.com/plus/img/logos/158x48/transistor-logo-gray-900.svg"
+            alt="hp"
+            src="https://static.vecteezy.com/system/resources/previews/020/190/465/non_2x/hp-logo-hp-icon-free-free-vector.jpg"
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
           />
           <img
-            alt="Reform"
-            src="https://tailwindui.com/plus/img/logos/158x48/reform-logo-gray-900.svg"
+            alt="Apple"
+            src="https://cdn.pixabay.com/photo/2018/05/08/21/08/apple-3383931_1280.png"
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
           />
           <img
-            alt="Tuple"
-            src="https://tailwindui.com/plus/img/logos/158x48/tuple-logo-gray-900.svg"
+            alt="Lenovo"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO_XNFrpHsStNfX5Hcqy1cenKDyNYxfFJMsA&s"
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
           />
           <img
-            alt="SavvyCal"
-            src="https://tailwindui.com/plus/img/logos/158x48/savvycal-logo-gray-900.svg"
+            alt="Beats"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDgwH-5RTvc0cHl_4Uze2n2ljt5l2gEZbvQg&s"
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
           />
           <img
-            alt="Statamic"
-            src="https://tailwindui.com/plus/img/logos/158x48/statamic-logo-gray-900.svg"
+            alt="Logitech"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-v6Fcsc6HpyPy5j1l7mc_xAapf1FhtvhZOw&s"
             width={158}
             height={48}
             className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
           />
+        
         </div>
       </div>
     </div>
