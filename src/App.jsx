218808c9f2c1ba5ebc2470ea99ccc9c7b82c7085
Leon-kings <leonakingeneye2002@gmail.com>
2025-01-26 17:21:16 -0800

    changes on logout

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ import CreateTestimonyUser from "./components/dashboard/user/components/testimon
 import CreatePostUser from "./components/dashboard/user/components/post/CreatePostUser";
 import UProfile from "./components/dashboard/user/components/profile/Profile";
 import Details from "./components/cart/appliances/components/Details";
-import LogOut from "./components/logs/Logout";
+import Log from "./components/logs/Logout";
 
 export default function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -118,7 +118,7 @@ export default function App() {
               path="/LogOut"
               element={
                 <PrivateRoute isAuthenticated={isAuthenticated}>
-                  <LogOut />
+                  <Log />
                 </PrivateRoute>
               }
             />
