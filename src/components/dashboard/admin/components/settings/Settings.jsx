580d8c9ed9ba70f802665aa5cb374c6d0b0e9b27
Leon-kings <leonakingeneye2002@gmail.com>
2025-02-03 19:46:40 -0800

    changes on responsive of settings

--- src/components/dashboard/admin/components/settings/Settings.jsx
+++ src/components/dashboard/admin/components/settings/Settings.jsx
@@ -17,28 +17,28 @@ export default function Settings() {
       <div className="title"></div>
       <div className="bg-white">
         <div className="grid bg-white py-4 grid-cols-[repeat(auto-fit,minmax(150px,1fr))] gap-4">
-          <div className="bg-blue-200 text-white p-4 text-center">
+          <div className="bg-blue-200 text-white p-4">
             <Link to={"/5040/Create/User/245678"}>
               <button>
                 <BiUserPlus />
               </button>
             </Link>
           </div>
-          <div className="bg-green-200 text-white p-4 text-center">
+          <div className="bg-green-200 text-white p-4">
             <Link to={"/5040/Create/Message/245678"}>
               <button>
                 <LuMessageCirclePlus />
               </button>
             </Link>
           </div>
-          <div className="bg-red-200 text-white p-4 text-center">
+          <div className="bg-red-200 text-white p-4">
             <Link to={"/5040/Create/Testimony/245678"}>
               <button>
                 <TbSettingsAutomation />
               </button>
             </Link>
           </div>
-          <div className="bg-yellow-200 text-white p-4 text-center">
+          <div className="bg-yellow-200 text-white p-4">
             <Link to={"/5040/Create/Post/245678"}>
               <button>
                 <BiCartAdd />
@@ -50,7 +50,7 @@ export default function Settings() {
         {/*  */}
         {/*  */}
         <div className="grid bg-white py-4 grid-cols-[repeat(auto-fit,minmax(150px,1fr))] gap-4">
-          <div className="bg-blue-200 text-white p-4 text-center">
+          <div className="bg-blue-200 text-white p-4">
             <Link to={"/5010/Userview"}>
               <button>
                 <EyeIcon />
@@ -58,7 +58,7 @@ export default function Settings() {
               </button>
             </Link>
           </div>
-          <div className="bg-green-200 text-white p-4 text-center">
+          <div className="bg-green-200 text-white p-4">
             <Link to={"/5010/Message"}>
               <button>
                 <EyeIcon />
@@ -66,7 +66,7 @@ export default function Settings() {
               </button>
             </Link>
           </div>
-          <div className="bg-red-200 text-white p-4 text-center">
+          <div className="bg-red-200 text-white p-4">
             <Link to={"/5010/Testimony"}>
               <button>
                 <EyeIcon />
@@ -74,7 +74,7 @@ export default function Settings() {
               </button>
             </Link>
           </div>
-          <div className="bg-yellow-200 text-white p-4 text-center">
+          <div className="bg-yellow-200 text-white p-4">
             <Link to={"/5010/Postview"}>
               <button>
                 <EyeIcon />
@@ -88,11 +88,11 @@ export default function Settings() {
         <h2 className="text-blue-300 bg-amber-100 rounded-md">
           Make your Components Compatible
         </h2>
-        <div className="grid bg-white grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-4">
-          <div className="text-white p-6 text-center">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+          <div className="text-white p-4">
             <CreateUser />
           </div>
-          <div className="text-white p-6 text-center">
+          <div className="text-white">
             <CreatePost />
           </div>
         </div>
@@ -104,11 +104,11 @@ export default function Settings() {
           We can make our Comments and Testimony and also create user and post
           or post products
         </p>
-        <div className="grid bg-white py-4 grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-4">
-          <div className="text-white p-6 text-center">
+        <div className="grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+          <div className="text-white p-6">
             <CreateMessage />
           </div>
-          <div className="text-white p-6 text-center">
+          <div className="text-white p-6">
             <CreateTestimony />
           </div>
         </div>
